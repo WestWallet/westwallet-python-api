f5080fca98e79d79762dc1a73e0dec37d2ac628b
WestWallet <admin@westwallet.info>
2019-11-09 22:53:31 +0200

    json dumps doublequotes fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='westwallet_api',
-    version='1.1.1',
+    version='1.1.2',
     install_requires=[
         "dataclasses==0.6",
         "requests==2.22.0"
