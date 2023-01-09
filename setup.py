962470223755609d14365ab0e278a3d3fc6ae337
WestWalletDev <55498764+westwallet-dev@users.noreply.github.com>
2023-01-09 12:02:32 +0200

    v1.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='westwallet_api',
-    version='1.1.5',
+    version='1.1.6',
     install_requires=[
         "dataclasses==0.6",
         "requests==2.22.0"
